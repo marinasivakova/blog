58819187c8449bb8a22a0fb2594c26f50a8b2288
marinasivakova <marinasivakova1799@gmail.com>
2024-01-25 18:07:44 +0300

    initial signup page css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
-//import Article from "../article/article";
 import Article from "../article/article";
 import ArticlesList from "../articlesList/articlesList";
 import Header from "../header/header";
+import SignUpPage from "../signUpPage/signUpPage";
 import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -14,6 +14,7 @@ const App = () => {
           <Route exact path="/" element={<ArticlesList />} />
           <Route exact path="/articles" element={<ArticlesList />} />
           <Route path="/articles/:slug" element={<Article />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="*" element={<ArticlesList />} />
         </Routes>
       </BrowserRouter>
